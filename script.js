9dcc90640052ea287ad711692ea7dec6b5e36a4b
ah-an-123 <117673551+ah-an-123@users.noreply.github.com>
2023-02-08 15:21:00 +0500

    update js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let back=document.getElementById("back");
 let clear=document.getElementById("clear")
 let userName=document.getElementsByClassName("userName")
+let btn=document.getElementsByClassName("btn")
+let arr=[];
 let name=prompt("Enter Your Name Please ");
  if(name==null){
     (Array.from(userName)).forEach((element)=>{
@@ -11,39 +13,91 @@ let name=prompt("Enter Your Name Please ");
 {(Array.from(userName)).forEach((element)=>{
     element.innerHTML=name;
 })}
+setTimeout(()=>{
+    alert("Woring On It Use Your Own Keybord For Writing Alphabets")
+},5000)
 
 let textArea=document.getElementById("textArea");
 document.getElementById("zero").addEventListener('click',()=>{
-    textArea.value=(textArea.value+0);
+    textArea.value=(textArea.value+"a");
 })
 
 document.getElementById("one").addEventListener('click',()=>{
-    textArea.value=(textArea.value+1);
+    textArea.value=(textArea.value+"b");
 })
 document.getElementById("two").addEventListener('click',()=>{
-    textArea.value=(textArea.value+2);
+    textArea.value=(textArea.value+"c");
 })
 document.getElementById("three").addEventListener('click',()=>{
-    textArea.value=(textArea.value+3);
+    textArea.value=(textArea.value+"d");
 })
 document.getElementById("four").addEventListener('click',()=>{
-    textArea.value=(textArea.value+4);
+    textArea.value=(textArea.value+"e");
 })
 document.getElementById("five").addEventListener('click',()=>{
-    textArea.value=(textArea.value+5);
+    textArea.value=(textArea.value+"f");
 })
 document.getElementById("six").addEventListener('click',()=>{
-    textArea.value=(textArea.value+6);
+    textArea.value=(textArea.value+"g");
 })
 document.getElementById("seven").addEventListener('click',()=>{
-    textArea.value=(textArea.value+7);
+    textArea.value=(textArea.value+"h");
 })
 document.getElementById("eight").addEventListener('click',()=>{
-    textArea.value=(textArea.value+8);
+    textArea.value=(textArea.value+"i");
 })
 document.getElementById("nine").addEventListener('click',()=>{
-    textArea.value=(textArea.value+9);
+    textArea.value=(textArea.value+"j");
 })
+document.getElementById("k").addEventListener('click',()=>{
+    textArea.value=(textArea.value+"k");
+})
+document.getElementById("l").addEventListener('click',()=>{
+    textArea.value=(textArea.value+"l");
+})
+document.getElementById("m").addEventListener('click',()=>{
+    textArea.value=(textArea.value+"m");
+})
+document.getElementById("n").addEventListener('click',()=>{
+    textArea.value=(textArea.value+"n");
+})
+document.getElementById("o").addEventListener('click',()=>{
+    textArea.value=(textArea.value+"o");
+})
+document.getElementById("t").addEventListener('click',()=>{
+    textArea.value=(textArea.value+"t");
+})
+document.getElementById("s").addEventListener('click',()=>{
+    textArea.value=(textArea.value+"s");
+})
+document.getElementById("r").addEventListener('click',()=>{
+    textArea.value=(textArea.value+"r");
+})
+document.getElementById("q").addEventListener('click',()=>{
+    textArea.value=(textArea.value+"q");
+})
+document.getElementById("p").addEventListener('click',()=>{
+    textArea.value=(textArea.value+"p");
+})
+document.getElementById("u").addEventListener('click',()=>{
+    textArea.value=(textArea.value+"u");
+})
+
+document.getElementById("v").addEventListener('click',()=>{
+    textArea.value=(textArea.value+"v");
+})
+
+document.getElementById("w").addEventListener('click',()=>{
+    textArea.value=(textArea.value+"w");
+})
+document.getElementById("x").addEventListener('click',()=>{
+    textArea.value=(textArea.value+"x");
+})
+document.getElementById("y").addEventListener('click',()=>{
+    textArea.value=(textArea.value+"y");
+})
+
+
 back.addEventListener('click',()=>{
 let b=Array.from(textArea.value)
 b.pop();
@@ -72,7 +126,3 @@ let up=document.getElementById("UpperCase")
         let u=textArea.value.toString()
         textArea.value=u.toLowerCase();
     })
-
-    setTimeout(()=>{
-        alert("Woring On It Use Your Own Keybord For Writing Alphabets")
-    },5000)
